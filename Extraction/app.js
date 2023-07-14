be30b4e85efd0edc681652495723a4000d19cb0d
BenjaminMoran1 <benjaminmorangg@gmail.com>
2023-07-13 20:52:30 -0400

    pushing finished code

---
+++ Extraction/app.js
@@ -0,0 +1,129 @@
+var url = "https://raw.githubusercontent.com/BigSuez/project-3/main/Extraction/Outputs/features.json";
+
+d3.json(url).then(function(data) {
+  console.log(data);
+
+  // Get the unique dates from the data
+  var dates = data.map(function(obj) {
+    return obj.date;
+  });
+
+  // Populate the dropdown with the dates
+  var dropdown = d3.select("#periodDropdown");
+  dropdown.selectAll("option")
+    .data(dates)
+    .enter()
+    .append("option")
+    .text(function(d) {
+      return d;
+    });
+
+  // Define the color scale for different features
+  var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+  // Function to update the chart based on the selected date
+  function updateChart(selectedDate) {
+    // Find the data object for the selected date
+    var selectedData = data.find(function(obj) {
+      return obj.date === selectedDate;
+    });
+
+    // Get the top 5 tracks for the selected date
+    var topTracks = selectedData.top_tracks.slice(0, 5);
+
+    // Define the legend data for each feature
+    var legendData = Object.keys(topTracks[0].features).filter(function(feature) {
+      return feature !== "duration_ms" && feature !== "time_signature" && feature !== "mode" && feature !== "key" && feature !== "tempo" && feature !== "loudness" && feature !== "instrumentalness" && feature !== "type" && feature !== "id" && feature !== "uri" && feature !== "track_href" && feature !== "analysis_url";
+    });
+
+    // Update the data traces and legend for the dot plot
+    var traces = [];
+    var legendItems = [];
+
+    topTracks.forEach(function(track) {
+      var trackTitle = track.track_title;
+      var features = track.features;
+
+      Object.keys(features).forEach(function(feature, index) {
+        if (feature !== "duration_ms" && feature !== "time_signature" && feature !== "mode" && feature !== "key" && feature !== "tempo" && feature !== "loudness" && feature !== "instrumentalness" && feature !== "type" && feature !== "id" && feature !== "uri" && feature !== "track_href" && feature !== "analysis_url") {
+          var value = features[feature];
+
+          var trace = {
+            x: [value],
+            y: [trackTitle],
+            mode: "markers",
+            marker: {
+              size: 10,
+              color: colorScale(index)
+            },
+            text: feature + ": " + value,
+            type: "scatter",
+            orientation: "h",
+            showlegend: false
+          };
+
+          traces.push(trace);
+        }
+      });
+    });
+
+    legendData.forEach(function(feature, index) {
+      var legendItem = {
+        name: feature,
+        marker: {
+          color: colorScale(index),
+          symbol: "circle"
+        }
+      };
+
+      legendItems.push(legendItem);
+    });
+
+    var layout = {
+      title: "Top 5 Tracks Per 6 months",
+      xaxis: {
+        title: "Feature Value",
+        range: [0, 1]
+      },
+      yaxis: {
+        title: "Track Title"
+      },
+      showlegend: false,
+      height: 400,
+      width: 1000,
+      margin: {
+        l: 300,
+        r: 50,
+        t: 200,
+        b: 50
+      }
+    };
+
+    Plotly.newPlot("chart", traces, layout);
+
+    // Create custom legend
+    var legendHtml = "";
+    legendItems.forEach(function(item) {
+      var color = item.marker.color;
+      var name = item.name;
+      
+      // Check for specific features and update the text color accordingly
+      if (name === "danceability" || name === "energy" || name === "speechiness" || name === "acousticness" || name === "liveness" || name === "valence") {
+        legendHtml += `<div class="legend-item"><span class="legend-marker" style="background-color:${color}"></span><span class="legend-label" style="color:${color}">${name}</span></div>`;
+      } else {
+        legendHtml += `<div class="legend-item"><span class="legend-marker" style="background-color:${color}"></span><span class="legend-label">${name}</span></div>`;
+      }
+    });
+    var legendContainer = document.getElementById("legend");
+    legendContainer.innerHTML = legendHtml;
+  }
+
+  // Initial chart update with the first date in the data
+  updateChart(data[0].date);
+
+  // Event listener for dropdown change
+  dropdown.on("change", function() {
+    var selectedDate = d3.select(this).property("value");
+    updateChart(selectedDate);
+  });
+});
